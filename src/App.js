273e3a7f510ec8accda05b164937ed2b905f185c
Кирилл Радостев <Radostevk80@yandex.ru>
2022-10-12 22:48:25 +0500

    modified:   src/App.js 	modified:   src/index.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import { useEffect } from 'react';
 import './App.css';
 import Product from './components/Product/Product.jsx'
+import Header from '/.components/Header/Header.jsx'
+import { Route, Routes } from 'react-router-dom'
+
 
 const tg = window.Telegram.WebApp;
 function App() {
@@ -14,10 +17,13 @@ function App() {
   }
 
   return (
-    <div>
-      <h1>Hello13</h1>
-      <button onClick={onClose}>Закрыть</button>
-      <Product />
+    <div className='App'>
+      <Header />
+      <Routes>
+        <Route index element={<Product />} />
+        <Route path={'form'} element={< Form />} />
+      </Routes>
+
     </div>
   )
 }
