ea44e54da4b065980b9dfaea2add2bd8f7e312be
Кирилл Радостев <Radostevk80@yandex.ru>
2022-10-12 22:02:40 +0500

    modified:   src/App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
+import { useEffect } from 'react';
 import './App.css';
 
+const tg = window.Telegram.WebApp();
+
 function App() {
+
+  useEffect(() => {
+    tg.ready();
+  }, [])
+
+  const onClose = () => {
+    tg.close();
+  }
+
   return (
     <div>
       <h1>Hello13</h1>
+      <button onClick={onClose}>Закрыть</button>
     </div>
   )
 }
