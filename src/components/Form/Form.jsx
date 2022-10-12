3473ffb41b69d0030ad28b6ed01fdc1184247059
Кирилл Радостев <Radostevk80@yandex.ru>
2022-10-12 22:32:33 +0500

    renamed:    src/components/Form/From.jsx -> src/components/Button/Button.css 	modified:   src/components/Button/Button.jsx 	renamed:    src/components/Product/Producr.jsx -> src/components/Form/Form.css 	new file:   src/components/Form/Form.jsx 	new file:   src/components/Header/Header.css 	modified:   src/components/Header/Header.jsx 	new file:   src/components/Product/Product.css 	new file:   src/components/Product/Product.jsx

---
+++ src/components/Form/Form.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './Form.css'
+
+const Form = () => {
+    <div>
+
+    </div>
+}
+
+export default Form;
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './Header.css'
+
+const Header = () => {
+    <div>
+
+    </div>
+}
+
+export default Header;
